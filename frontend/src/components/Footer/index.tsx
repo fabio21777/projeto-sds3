3614e1ffd9aec51eaf2e2cfe7d32e660e9e61b9a
fabio21777 <fabiosouza21777@gmail.com>
2021-09-12 17:48:20 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Fabio Miranda</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Repositorio do Github: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">https://github.com/fabio21777/projeto-sds3</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
-import 'assets/css/styles.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css';
 import App from './App';
 
 ReactDOM.render(
